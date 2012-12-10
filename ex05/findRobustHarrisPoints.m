c772b039e05332f4563facf27bd4675de7ef54d7
Daniel Malovetz <malovetz@in.tum.de>
2012-12-10 12:03:56 +0100

    Started warping/backwarping process
    I started the the warping/backwarping process for the robust feature
    points. At this stage, i produce random affine transformations  with the
    formula in the paper. The formula is written in line 31 of the code, the
    distribution of the angles/lambdas is taken from the paper, too.
    Then the patches around each harrisPoint are warped a number of times
    (backwarps times) and thus new patches are created. The regions of the
    patches where no warped pixel was projected at are then filled with
    random black&white noise, as seen in the lecture slides.
    I don't know if the code works like this and is correct and I'm not even
    sure if that's the right approach.
    At next I will look to finding new harrisPoints in the warped image
    patches and then backproject these points to find out if they are robust
    enough.
    I guess for the warping process for FERN training we can use most of the
    codes I am writing here.

--- ex05/findRobustHarrisPoints.m
+++ ex05/findRobustHarrisPoints.m
@@ -6,7 +6,7 @@ function [ resPoints ] = findRobustHarrisPoints( img , borderSize, maxPoints)
 % TODO: search for more than 5 harris-points
 harrisPoints = corner(img,'Harris',maxPoints)';
 
-[m,n] = size(img)
+[m,n] = size(img);
 
 
 % delete harrisPoints near border
@@ -26,6 +26,77 @@ for i=1:size(harrisPoints,2)
 end
 
 resPoints = resPoints(:,1:ctr);
+
+% warp patch around harrisPoints and see if harrisPoint is robust enough
+% R? R?? diag(?1,?2) R?
+%the rotation angles are randomly chosen from [0;2pi]
+%?1,?2 are randomly chosen from [0.6; 1.5]
+
+backwarps = 10;
+
+theta = (2*pi) * rand(backwarps);
+phi = (2*pi) * rand(backwarps);
+
+lambdas = 0.6 + 0.9 * rand(2);
+
+%build the rotation/scale matrices
+rotationTheta = zeroes(backwarps, 2, 2);
+rotationPhi = zeroes(backwarps, 2, 2);
+invRotationPhi = zeroes(backwarps, 2, 2);
+scaling = zeroes(backwarps, 2, 2);
+
+for i=1:backwarps
+   
+    rotationTheta(i) = [cos(theta(i)) sin(theta(i));
+                    -sin(theta(i)) cos(theta(i))];
+                
+    rotationPhi(i) = [cos(phi(i)) sin(phi(i));
+                    -sin(phi(i)) cos(phi(i))];
+                
+    invRotationPhi(i) = [cos(phi(i)) -sin(phi(i));
+                    sin(phi(i)) cos(phi(i))];
+                
+    scaling(i) = [lambdas(1, i) 0;
+                0 lambdas(2, i)];
+
+end
+
+%now we use the matrices on every point in the patch of every harrisPoint
+%and build new image patches
+patches = -1* ones(harrisPoints, backwarps, 2*borderSize+1, 2*borderSize+1);
+for h = 1:size(harrisPoints)
+    x = harrisPoints(1,h); %not sure if correct (are harris corners stored as (x, y)T or (y, x)T ?
+    y = harrisPoints(2,h);
+    for b = 1:backwarps
+        for r = -borderSize:borderSize
+            for c = -borderSize:borderSize
+
+                point = img(y+r, x+c, 1);
+                transformed = rotationTheta(b) * rotationPhi(b) * scaling(b) * invRotationPhi(b) * transpose([y+r x+c]);
+                if (transformed(1) < y - borderSize && transformed(1) < y + borderSize && ...
+                    transformed(2) < x - borderSize && transformed(2) < x + borderSize)
+                    
+                    patches(h, b, transformed(1), transformed(2)) = point;
+                    
+                end
+
+            end
+        end
+
+        %produce noise for undefined image points (i.e. no point was warped
+        %to this position
+        for r = -borderSize:borderSize
+            for c = -borderSize:borderSize
+                if(patches(h, b, r, c) == -1)
+                   if(randi(2) == 1) patches(h, b, r, c) = 0;
+                   else patches(h, b, r, c) = 255;
+                   end
+                end
+            end
+        end
+    end
+    
+end
 
 
 % accHarrisPoints = zeros(size(img))
