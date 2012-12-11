6edf2bfc2cb352621e5d151e787e136eac6a8362
Daniel Malovetz <malovetz@in.tum.de>
2012-12-11 14:21:23 +0100

    Update

--- ex05/findRobustHarrisPoints.m
+++ ex05/findRobustHarrisPoints.m
@@ -8,7 +8,6 @@ harrisPoints = corner(img,'Harris',maxPoints)';
 
 [m,n] = size(img);
 
-
 % delete harrisPoints near border
 resPoints = zeros(2,size(harrisPoints,2));
 ctr = 0;
@@ -27,86 +26,18 @@ end
 
 resPoints = resPoints(:,1:ctr);
 
-% warp patch around harrisPoints and see if harrisPoint is robust enough
-% R? R?? diag(?1,?2) R?
-%the rotation angles are randomly chosen from [0;2pi]
-%?1,?2 are randomly chosen from [0.6; 1.5]
 
-backwarps = 10;
-
-theta = (2*pi) * rand(backwarps, 1);
-phi = (2*pi) * rand(backwarps, 1);
-
-lambdas = 0.6 + 0.9 * rand(backwarps, 2);
-
-%build the rotation/scale matrices
-rotationTheta = zeros(2, 2, backwarps);
-rotationPhi = zeros(2, 2, backwarps);
-invRotationPhi = zeros(2, 2, backwarps);
-scaling = zeros(2, 2, backwarps);
-
-for i=1:backwarps
-   
-    rotationTheta(:,:,i) = [cos(theta(i)) sin(theta(i));
-                            -sin(theta(i)) cos(theta(i))];
-                
-    rotationPhi(:,:,i) = [cos(phi(i)) sin(phi(i));
-                        -sin(phi(i)) cos(phi(i))];
-                
-    invRotationPhi(:,:,i) = [cos(phi(i)) -sin(phi(i)); 
-                            sin(phi(i)) cos(phi(i))];
-                
-    scaling(:,:,i) = [lambdas(i, 1) 0;
-                    0 lambdas(i, 2)];
-
-end
-
-%now we use the matrices on every point in the patch of every harrisPoint
-%and build new image patches
-%patches = ones(size(resPoints, 1), backwarps, 2*borderSize+1, 2*borderSize+1) * -1;
-patches = ones(2*borderSize+1, 2*borderSize+1, backwarps, size(resPoints, 1)) * -1;
-
-for h = 1:size(resPoints, 1)
-    x = resPoints(1,h); %not sure if correct (are harris corners stored as (x, y)T or (y, x)T ?
-                        % stefan: it's correct. line 1 is x and line 2 is y
-    y = resPoints(2,h);
-
-    for b = 1:backwarps
-
-        tempPatch = img(y-borderSize : y+borderSize, x-borderSize : x+borderSize);
-
-        transformation = rotationTheta(:, :, b) * invRotationPhi(:, :, b) * scaling(:, :, b) * rotationPhi(:, :, b);
-        transformation = [transformation, [0;0]];
-        transformation = [transformation; [0 0 1]];
-        tform = maketform('affine', transformation);
-
-        transImage = imtransform(tempPatch, tform, 'XData', [1 2*borderSize+1], 'YData', [1 2*borderSize+1], 'FillValues', -1);
-        patches(:, :, b, h) = transImage;
-
-        
-        %produce noise for undefined image points (i.e. no point was warped
-        %to this position
-        for r = 1 : 2 * borderSize + 1
-            for c = 1 : 2 * borderSize + 1
-                if(patches(r, c, b, h) == -1)
-                   if(randi(2) == 1)
-                       patches(r, c, b, h) = 0;
-                   else
-                       patches(r, c, b, h) = 255;
-                   end
-                end
-            end
-        end
-    end
-end
-
-for i = 1:size(patches, 3)
-    for j = 1:size(patches, 4)
-        
-        figure(3);
-        imshow(patches(:,:,i,j), [0 255]);
-        
-    end
+for p = 1:size(resPoints, 1)
+    x = resPoints(1, p);
+    y = resPoints(2, p);
+    
+    [warpedPatches, transformations] = warpImagePatch(img(y-borderSize : y+borderSize, x-borderSize : x+borderSize), 20);
+    %[warpedPatches, transformations] = warpImagePatch(img, 10);
+    
+    for i = 1:size(warpedPatches, 3)
+        figure();
+        imshow(warpedPatches(:, :, i), [0 255]);
+    end  
 end
 
 
