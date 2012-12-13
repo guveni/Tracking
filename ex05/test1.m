a0140aa2bd0742bec2fcd9c41f9adfb7e106d379
steckl <stefan-stoeckl@gmx.de>
2012-12-13 01:17:03 +0100

    some changes but still not good

--- ex05/test1.m
+++ ex05/test1.m
@@ -1,36 +1,64 @@
 % reading the image
 
 I=imread('imagesequence/img1.ppm');
+I = double(rgb2gray(I)) ;
 
+[sY sX] = size(I);
 
-% I = I(1:20,1:20);
-% 
-% imshow(I);
-H = [3 1 1; 
-     -5 2 0; 
-     2 3 1];
-
-H2 = [-2 0 0; 
-      0 2 0; 
-      100 0 1];
+H2 = [1 1 0; 
+      -1 1.0 0; 
+      0 0 1];
+%  
+% H2 = [.5 .5 0; 
+%       -.5 .5 0; 
+%       0 0 1];
  
-p1 = [1;1;1];
-p2 = [p1(1)-400
-p2 = H * p1;
-
 
 
 tform = maketform('affine',H2);
-[J,xdata,ydata] = imtransform(I,tform);
+[WARP,xdata,ydata] = imtransform(I,tform);
+
+[swY swX] = size(WARP);
+% 
+% p1 = [25;50;1];
+% p1rel = p1 - [sX/2;sY/2;0];
+% 
+% p2rel = inv(H2) * p1rel;
+% p2 = p2rel + [swX;swY;0];
+
+%p2rel = inv(H2) * [-400;-320;1];
+
+p2 = [swX/2;swY/2;1];
+p2 = [400;400;1];
+p2rel = p2 - [swX/2;swY/2;0];
+
+p1rel = H2 * p2rel;
+p1 = p1rel + [sX/2;sY/2;0];
+
+p11 = [sX/2;1;1];
+% p11 = [1;1;1];
+p11rel = p11 - [sX/2;sY/2;0];
+
+p22rel = (H2) \ p11rel;
+p22 = p22rel + [swX/2;swY/2;0];
+
+center = [sX/2,sY/2];
 figure(1);
-imshow(I);
+imshow(I,[0 255]);
 hold on;
-plot(p1(1),p1(2),'xg');
+plot([center(1),p1(1)],[center(2),p1(2)],'-xr');
+plot([center(1),p11(1)],[center(2),p11(2)],'-xy');
+plot(sX/2,sY/2,'og');
 hold off;
+
+centerW = [swX/2,swY/2];
 figure(2);
-imshow(J,'XData',xdata,'YData',ydata);
+imshow(WARP,[0 255]);
 hold on;
-plot(p2(1)+xdata,p2(2)+ydata,'xg');
+plot([centerW(1),p2(1)],[centerW(2),p2(2)],'-xr');
+plot([centerW(1),p22(1)],[centerW(2),p22(2)],'-xy');
+plot(p22(1),p22(2),'xy');
+plot(swX/2,swY/2,'og');
 hold off;
 
 
