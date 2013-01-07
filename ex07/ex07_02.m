f1757b6b6d1b051eb7b478677addbc89e557b44a
Daniel Malovetz <malovetz@in.tum.de>
2013-01-07 20:42:00 +0100

    Implemented integral images

---
+++ ex07/ex07_02.m
@@ -0,0 +1,7 @@
+img1 = imread('./test_images/face1.jpg');
+img2 = imread('./test_images/face2.jpg');
+img3 = imread('./test_images/face3.jpg');
+
+intImg1 = constructIntegralImage(img1);
+intImg2 = constructIntegralImage(img2);
+intImg3 = constructIntegralImage(img3);
